7893a4e0fc100e3a3b1fa35dcc0639742a69ffd6
phoebeoliff <phoebeoliff@gmail.com>
2023-10-03 12:10:05 +1300

    experiments1
    playing with shapes and background colour

--- sketch.js
+++ sketch.js
@@ -16,28 +16,45 @@ function setup () {
   let main_canvas = createCanvas(1920, 1080);
   main_canvas.parent('canvasContainer');
 
-  imageMode(CENTER);
+  // imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  background(255, 227, 150); //goldenyellow
+  // image(sourceImg, 0, 0, width, height);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<4000;i++) {
+  for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
+    stroke(pix); 
+
+
     if(mask[0] > 128) {
+      // // pix[1] = 0;
+      // // pix[2] = 0;
       let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
+      fill(pix); 
+    
+      rect(x, y, pointSize, pointSize);
+      // strokeWeight(6); 
+      // line(x-10, y, x+10, y);
     }
     else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+      // pix[0] = 0;
+      // pix[2] = 0;
+      // fill(pix); 
+      // fill(pix);
+      // let pointSize = 10;
+      // rect(x, y, pointSize, pointSize);  
+      strokeWeight(2); 
+      line(x-10, y-10, x+10, y+10);
     }
+
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
